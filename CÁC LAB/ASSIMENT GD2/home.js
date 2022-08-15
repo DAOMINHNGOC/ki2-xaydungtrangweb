c2c72d9dc7511f31f4065656c381adea9d44a430
minhngoc <ngodmph20534@fpt.edu.vn>
2022-08-15 17:23:11 +0700

    first upload codet

---
+++ CÁC LAB/ASSIMENT GD2/home.js
@@ -0,0 +1,33 @@
+var slideIndex = 0;
+showSlide();
+
+function plusSlides(n) {
+    showSlide2((slideIndex += n));
+}
+
+function showSlide2(n) {
+    var slides = document.getElementsByClassName("one");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+function showSlide() {
+    var slides = document.getElementsByClassName("one");
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlide, 5000);
+}
